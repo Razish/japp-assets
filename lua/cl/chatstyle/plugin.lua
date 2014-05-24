9b283eb676790e2afe7def15e625edea387edb5c
Razish <mrrazish@gmail.com>
2014-05-25 00:50:47 +1000

    Fix bug in chatstyle plugin

--- lua/cl/chatstyle/plugin.lua
+++ lua/cl/chatstyle/plugin.lua
@@ -1,8 +1,11 @@
-local chatstyle = RegisterPlugin( "ChatStyle", "1.0" )
+local chatstyle = RegisterPlugin( "ChatStyle", "1.1" )
+
+CreateCvar( 'cg_chatPrefix', '', CvarFlags.ARCHIVE )
+CreateCvar( 'cg_chatSuffix', '', CvarFlags.ARCHIVE )
 
 AddListener( "JPLUA_EVENT_CHATMSGSEND", function( msg, mode, target )
-	if string.len( msg ) then return msg end
-	local prefix = GetCvar('cg_chatPrefix')
-	local suffix = GetCvar('cg_chatSuffix')
+	if string.len( msg ) == 0 then return msg end
+	local prefix = GetCvar( 'cg_chatPrefix' )
+	local suffix = GetCvar( 'cg_chatSuffix' )
 	return (prefix and prefix:GetString() or '') .. msg .. (suffix and suffix:GetString() or '')
 end )
