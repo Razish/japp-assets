33c2a826e26826b75de8373b139474a6718df311
EpicLoyd <boriscool007@gmail.com>
2016-01-15 23:31:01 +1000

    iplog fix

--- lua/sv/iplog/plugin.lua
+++ lua/sv/iplog/plugin.lua
@@ -4,14 +4,6 @@ local plugin = RegisterPlugin( 'iplog', '0.2.0', '13.0.0' )
 local iplist = {}
 local logname = 'ips.json'
 
-local function Load()
-	local sr = GetSerialiser( logname, FSMode.Read )
-	iplist = sr:ReadTable( 'iplist' )
-	sr:Close()
-	sr = nil
-end
-Load()
-
 local function Save()
 	print( 'Saving IP log...' )
 	local sr = GetSerialiser( logname, FSMode.Write )
@@ -20,6 +12,19 @@ local function Save()
 	sr:Close()
 	sr = nil
 end
+
+
+local function Load()
+	local sr = GetSerialiser( logname, FSMode.Read )
+	if not sr then
+		Save()
+		return
+	end
+	iplist = sr:ReadTable( 'iplist' )
+	sr:Close()
+	sr = nil
+end
+Load()
 
 AddServerCommand( 'iplog', function( args )
 	if #args == 0 then
